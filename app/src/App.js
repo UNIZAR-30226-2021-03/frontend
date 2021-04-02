b2ccda845a50e7ebf354d81641a124136dfb706b
Javier Vela <javier.vela00@gmail.com>
2021-04-02 22:08:12 +0200

    cambios App index y demas

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import Home from './components/pages/Home';
 import LogIn from './components/pages/LogIn';
+import SignUp from './components/pages/SignUp';
+import Landing from './components/pages/Landing';
 import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -11,9 +13,11 @@ const App = () => {
     <Router>
       <Navbar/>
       <Switch>
-          <Route path='/' exact component={Home}></Route>
-          <Route path='/log-in' exact component={LogIn}></Route>
-        </Switch>
+          <Route path='/' exact component={Landing}></Route>
+          <Route path='/home' exact component={Home}></Route>
+          <Route path='/login' exact component={LogIn}></Route>
+          <Route path='/signup' exact component={SignUp}></Route>
+        </Switch>   
     </Router>
   );
 }
