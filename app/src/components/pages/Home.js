231b38266d39e739aed236b65e69eb8f2fdad5ef
Javier Vela <javier.vela00@gmail.com>
2021-04-02 22:05:10 +0200

    Modificación de página principal

--- app/src/components/pages/Home.js
+++ app/src/components/pages/Home.js
@@ -1,23 +1,34 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import prueba from '../../services/prueba'
+import { Container, CssBaseline } from '@material-ui/core';
+
+
 const Home = () => {
+
     const [data, setData] = useState({})
     useEffect(() => {
         start()
-
-        async function start(){
-        console.log('hola')
-        const res = await  prueba()
-        console.log(res)
-         setData(res)
+        async function start() {
+            console.log('hola')
+            const res = await prueba()
+            console.log(res)
+            setData(res)
         }
+    }, [])
 
-    },[])
-    return( 
-        <div>
-            ESTÁS EN EL HOME 
+    return (
+        <Container component='main' maxWidth='xl'>
+
+            <div className="Home">
+                <h1> hola </h1>
+
+            HOMEEE
+            Hllo
+
+            # el container y/o CSSBaseline modifican la altura y padding de los links de nacbar
             {data.user}
-        </div>
+            </div>
+        </Container>
     )
 }
 
