30e23e9de3876c628004960e05ddcd3d82728054
JorgeBernalRomero <JorgeBernalRomero@users.noreply.github.com>
2021-04-06 20:18:31 +0200

    Ultimos cambios footer

--- app/src/components/footer/Footer.js
+++ app/src/components/footer/Footer.js
@@ -4,11 +4,12 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles, withTheme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import {DataGrid} from '@material-ui/data-grid';
 
 function Copyright() {
   //textAlign();
   return (
-    <Typography variant="body2" color="textSecondary">
+    <Typography variant="body2" color='white' align="right">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
         Barbaros Software
@@ -36,7 +37,8 @@ const useStyles = makeStyles((theme) => ({
   footer: {
     padding: theme.spacing(3, 2),
     marginTop: 'auto',
-    backgroundColor: 'rgb(232, 238, 241)'
+    backgroundColor: 'rgb(30, 61, 88)',
+    color: 'rgb(232, 238, 241)'
   },
 }));
 
@@ -49,7 +51,19 @@ export default function StickyFooter() {
       </Container>
       <footer className={classes.footer}>
         <Container maxWidth="sm">
-          <Typography variant="body1">My sticky footer can be found here.</Typography>
+          <Typography variant="body1" align="right">My sticky footer can be found here.</Typography>
+          <DataGrid
+            columns={[
+              { field: 'id', hide: true },
+              {
+                field: 'username',
+                headerName: 'Username',
+                description:
+                  'The identification used by the person with access to the online service.',
+              },
+              { field: 'age', headerName: 'Age' },
+            ]}
+          />
           <Copyright />
         </Container>
       </footer>
