d6952df2d8c91753c43800936bbad4d683627df2
Javier Vela <javier.vela00@gmail.com>
2021-04-07 18:07:19 +0200

    Login - interaccion backend

--- app/src/components/pages/LogIn.js
+++ app/src/components/pages/LogIn.js
@@ -2,11 +2,17 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import login from '../../services/Login.service.js';
+import sendLogIn from '../../services/LogIn.service.js';
+import send2FA from '../../services/2FA.service.js';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -55,15 +61,23 @@ const LogIn = () => {
 
   const [failAuthPassword, setFailAuthPassword] = React.useState(false);
 
+  const [failAuth2FA, setFailAuth2FA] = React.useState(false);
+
   const [mail, setMail] = React.useState("");
 
   const [password, setPassword] = React.useState("");
 
-  const emailRegEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  const [open2FA, setOpen2FA] = React.useState(false);
+
+  const [code2FA, setCode2FA] = React.useState("");
+
+  const [loginToken, setLoginToken] = React.useState("aa");
+
+  const mailRegEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
   const onChangeMail = (e) => {
     setMail(e.target.value);
-    if (!emailRegEx.test(e.target.value)) { // TODO alguna comprobación más ?
+    if (!mailRegEx.test(e.target.value)) { // TODO alguna comprobación más ?
       setErrorMail(true);
     } else {
       setErrorMail(false);
@@ -79,23 +93,68 @@ const LogIn = () => {
     }
   }
 
-  const handleLogin = () => {
-    if(errorPassword || errorMail) { // TODO alguna comprobación más ?      
-      console.log("ERROR")
-    } else {
+  const onChangeCode2FA = (e) => {
+    setFailAuth2FA(false)
+    setCode2FA(e.target.value);
+  }
+
+  const handleLogIn = () => {
+    var noErrors = true
+    if (!mailRegEx.test(mail)) { // TODO alguna comprobación más
+      setErrorMail(true)
+      noErrors = false
+    }
+    if (password === "") {
+      setErrorPassword(true)
+      noErrors = false
+    }
+    if (noErrors) {
       // Si no hay errores con los datos introducidos procedemos a log-in
       // TODO Puede renderizarse una animación mientras se hace la petición...
       // TODO realizar petición
-      console.log("HOLA HOAL")
-      login()
-      .then( res => {
-        console.log(res)
-      })
-      .catch( err => {
-        console.log("Errrorrrrr")
-      })
+      const response = sendLogIn(mail, password)
+        .then(response => {
+          // TODO modificar errores visuales dependiendo del código de error
+          // TODO que pasa si es un codigo de erro no conocido
+          if (response === 400) { // no se cumplen los requirements 
+            console.log("--- ERROR " + response + ": !")
+          } else if (response === 401) {
+            console.log("--- ERROR " + response + ": !")
+          } else if (response === 404) {
+            console.log("--- ERROR " + response + ": !")
+          } else if (response === 500) {
+            console.log("--- ERROR " + response + ": !")
+          } else if (response === 501) {
+            console.log("--- ERROR " + response + ": !")
+          } else {
+            setLoginToken(response._2faToken)
+            //console.log(response._2faToken)
+            //console.log(response)
+            //console.log(loginToken)
+            setOpen2FA(true)
+          }
+        })
       // TODO comprobar errores de peticion
     }
+  }
+
+  const handle2FA = () => {
+    // TODO implementar numero de intentos para introducir correctamente el codigo (por ahora infinitas)
+    console.log(loginToken)
+
+    const response = send2FA(loginToken, code2FA)
+      .then(response => {
+        if (response === 401) {
+          console.log("--- ERROR " + response + ": !")
+          setFailAuth2FA(true)
+        } else {
+          // TODO guardar el token de acceso
+          localStorage.setItem("accessToken", response.accessToken)
+          console.log(localStorage.getItem("accessToken"))
+          setOpen2FA(false)
+        }
+        setCode2FA("")
+      })
   }
 
   return (
@@ -159,10 +218,10 @@ const LogIn = () => {
         </Grid>
         <Grid item xs={12}>
           <Button
-            onClick={handleLogin}
+            onClick={handleLogIn}
             className={classes.submit}
             fullWidth="true"
-            type="submit"
+          //type="submit"
           >
             Log in
             </Button>
@@ -178,7 +237,37 @@ const LogIn = () => {
           </Link>
         </Grid>
       </form>
-    </Container>
+
+      <Dialog open={open2FA} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">2FA Verification Code</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Por favor, introduzca el código de verificación enviado a: {mail}
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="none"
+            label="Código"
+            type="password"
+            fullWidth="true"
+            required
+            size="large"
+            name="code2FA"
+            //id="code2FA"
+            onChange={onChangeCode2FA}
+            value={code2FA}
+            error={failAuth2FA}
+            helperText={failAuth2FA ? 'Code is incorrect' : ' '}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handle2FA} color="primary" fullWidth="true">
+            Enviar
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+    </Container >
   );
 }
 
