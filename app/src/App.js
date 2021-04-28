45f9cf0a2d36e6d5e420c0b3275abb7ac58098e8
Andoni Salcedo <andonisalcedo@gmail.com>
2021-04-28 20:36:38 +0200

    modulo contraseña

--- app/src/App.js
+++ app/src/App.js
@@ -9,11 +9,14 @@ import NavbarPublic from './components/navbar/NavbarPublic';
 import Footer from './components/footer/Footer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AuthContext from './context'
+import {generatePassword} from './helpers/password.helper'
 const App = () => {
 
   const [accessToken, setAccessToken] = useState(null)
 
   useEffect(() => {
+    const password = generatePassword(16,true,true,true,"_-.");
+    console.log(password)
     setAccessToken(localStorage.getItem('accessToken'))
   }, [])
 
