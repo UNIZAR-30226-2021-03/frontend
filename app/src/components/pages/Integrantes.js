c58d1c9aa88bfb4813b4e8ca55778fb4461579ad
JorgeBernalRomero <JorgeBernalRomero@users.noreply.github.com>
2021-05-16 22:43:50 +0200

    terminada pantalla de quiénes somos

--- app/src/components/pages/Integrantes.js
+++ app/src/components/pages/Integrantes.js
@@ -1,20 +1,19 @@
-import { Box } from '@material-ui/core';
+import { Box, Tab } from '@material-ui/core';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import keyImage from '../../resources/key-1315566.jpg';
-
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
     integrantesText: {
+        textAlign: 'center',
         position: 'fixed',
         top: '15%',
         left: '50%',
         right: '50%',
-        width: '1000px',
+        width: '1500px',
         transform: 'translate(-50%, -50%)',
         flexGrow: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
     },
 
     title: {
@@ -24,26 +23,9 @@ const useStyles = makeStyles((theme) => ({
     },
     description: {
         fontSize: '30px',
-        textAlign: 'center',
+        textAlign: 'justify',
         position: 'fixed',
     },
-
-    container: {
-        /* The image used */
-        backgroundImage: 'url('+ keyImage +')',
-
-        /* Full height */
-        minHeight: '80vh', // TODO mal, hacer relativo (COMO??)
-        /*width: 100%, */
-        margin: '0',
-
-        /* Center and scale the image nicely */
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-
-        flex:'1'
-    }
 }));
 
 const Integrantes = () => {
@@ -51,21 +33,27 @@ const Integrantes = () => {
     const classes = useStyles();
 
     return (
-        //<Container component='div' maxWidth='xl' className='container'>
-            <Box component="div" className={classes.integrantesText}>
-                <Box component="div" className={classes.title}>
-                Quiénes somos
-                </Box>
-                <Box component="div" className={classes.description}>
-                Bárbaros Software S.A. cuenta con tres años de experiencia en el mercado. 
-                Destacamos nuestras habilidades en el mundo empresarial que nos avala como una empresa referente en el sector.
-                La empresa se caracteriza por una filosofía basada en el trabajo coordinado en grupo, con especialistas en distintos campos del sector que unificados forman un equipo de trabajo altamente sofisticado.
-                Los servicios y capacidades que nuestros clientes han podido destacar entre los más importantes son: <br/> <br/>
-                Poner la experiencia...
-                </Box>
+        <Box component="div" className={classes.integrantesText}>
+            <Box component="div" className={classes.title}>
+            Quiénes somos
             </Box>
-       //</Container>
-
+            <Box component="div" className={classes.description}>
+            Bárbaros Software S.A. cuenta con tres años de experiencia en el mercado. 
+            Destacamos nuestras habilidades en el mundo empresarial que nos avala como una empresa referente en el sector.
+            La empresa se caracteriza por una filosofía basada en el trabajo coordinado en grupo, con especialistas en distintos campos del sector que unificados forman un equipo de trabajo altamente sofisticado.
+            Entre los servicios y capacidades que nuestros clientes han podido destacar de nosotros están: <br/> <br/>
+            -Experiencia en desarrollo móvil en dispositivos Android.<br/>
+            -Veteranía en el framework de JavaScript React para Web y Móvil.<br/>
+            -Destreza en el entorno de ejecución NodeJS con el framework Express.<br/>
+            -Práctica en Bases de Datos relacionales (Oracle, Postgresql, MySql...).<br/>
+            -Conocimiento en Bases de Datos no relacionales (MongoDB). <br/>
+            -Alto uso de diferentes lenguajes de programación (Java, JavaScript, C, C++, GoLang).<br/>
+            -Estudio de metodología de despliegue de Proyectos con Docker y Kubernetes. <br/>
+            -Maestría en el uso de IaaS (Microsoft Azure, Amazon AWS).<br/>
+            -Prácticas de testing con PostMan.<br/>
+            -Altos conocimientos en gestión de versiones a través del Software Git.<br/>
+            </Box>
+        </Box>
     )
 }
 
